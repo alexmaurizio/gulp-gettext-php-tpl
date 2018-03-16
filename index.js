d2b995ec8d48f49fe9337c679bf52e3625dc1613
Alessandro Maurizio <alessandro.maurizio@mondadori.it>
2018-03-16 11:47:15 +0100

    Package cleanup & Big rework (concatenating single method)

--- index.js
+++ index.js
@@ -1,153 +1,40 @@
 // Module Loading
-var each        = require('gulp-each');
-var through     = require('through2');
-var PluginError = require('plugin-error');
 var path        = require('path');
+var through     = require('through2');
 var gtxParser   = require("gettext-parser");
 var File        = require('vinyl');
 var Concat      = require('concat-with-sourcemaps');
+var PluginError = require('plugin-error');
 var utils       = require('./src/helpers.js');
 
-// Consts
+// Const
 const PLUGIN_NAME = 'gulp-gettext-php-tpl';
 
-// Iterates each file and passes over a list of IDs for the GetText methods
-function parseAsStringList() {
-
-	// Creating a stream through which each file will pass
-	return each(function (content, file, callback) {
-
-		// Get the extension - .TPL are Smarty Templates, .PHP are classes
-		var ext = file.path.split('.').pop().toLowerCase();
-
-		// Check that the file is .php or .tpl
-		if (ext === "php" || ext === "tpl") {
-			// All translatable strings holder
-			var holder = "";
-
-			// Switch PHP and TPL
-			var regex = null;
-			if (ext === "tpl") {
-				// Regex for Smarty TPL Matching
-				regex = new RegExp('(?:{t)(?:.*?})(.*?)(?={\\/t})', 'g');
-			}
-			else {
-				// Regex for PHP Gettext Matching
-				regex = new RegExp('(?:_\\(\')(.*?)(?:\'\\)(?:;|,))', 'g');
-			}
-
-			// Iterate the matches
-			while ((match = regex.exec(content)) != null) {
-
-				// Get the string and make some string fixes to match original style
-				var string = match[1];
-				string = utils.stripslashes(string);
-				string = string.replace(/[\r\n]+/g, "");
-				string = string.replace(/\\([\s\S])|(")/g, "\\$1$2");
-
-				// Put into holder
-				holder += string + "\n";
-			}
-
-			// Remove last \n
-			holder = holder.substr(0, holder.length - 1);
-
-			// Pass the holder back and move to next file
-			callback(null, holder);
-		}
-		else {
-			// File is not a .tpl or .php - Ignore!
-			callback();
-		}
-
-	});
-
-}
-
-// Takes a (concatenated) list of ID Strings from the GetText Parser and pushes over a complete POT File
-function stringListToPOT() {
-	return through.obj(function (file, enc, cb) {
-
-		// Translate in array and sort alphabetically
-		var tradArray = file.contents.toString().split("\n").sort();
-
-		// Remove duplicates and empty ones
-		var uniques = {};
-		uniques = tradArray.filter(function (item) {
-			if (item === undefined) return false;
-			if (item === '') return false;
-			return uniques.hasOwnProperty(item) ? false : (uniques[item] = true);
-		});
-
-		// Build the POT File
-		var pot = utils.generatePotHeader() + '\nmsgid "' +
-				uniques.join('"\nmsgstr ""\n\nmsgid "')
-				+ '"\nmsgstr ""\n';
-
-		// Push the new file to the buffer, callback empty
-		var f = path.parse(file.path);
-		var newFile = file.clone();
-		newFile.contents = new Buffer(pot);
-		newFile.path = path.join(f.dir, f.name + f.ext);
-		this.push(newFile);
-		cb();
-
-	})
-}
-
-// Takes a potFile from the stream in and creates a .po file
-// Passes over the potFile to the stream
-function msgmerge(targetFile) {
-
-	if (!targetFile) {
-		throw new PluginError(PLUGIN_NAME, 'Target file path is required');
-	}
-
-	return through.obj(function (file, enc, callback) {
-
-		// Load the potFile from the stream
-		var potFile = gtxParser.po.parse(file.contents.toString());
-
-		// Makes PO and MO files from old PO and the new POT
-		utils.makePoMo(targetFile, potFile);
-
-		// Callback and passes over the original file
-		callback(null, file);
-
-	})
-}
-
-// Exporting the plugin main functions
-module.exports = {
-	parseAsStringList: parseAsStringList,
-	stringListToPOT: stringListToPOT,
-	msgmerge: msgmerge,
-	generatePOT: generatePOT
-}
-
-
-
-// Test
+// Main method to generate a POT file from a lot of files
+// Uses modified concatenation logic from gulp-concat
 function generatePOT(file) {
 	if (!file) {
-		throw new Error(PLUGIN_NAME+': Missing file option');
+		throw new PluginError(PLUGIN_NAME, 'Missing file option');
 	}
 
+	// Concat declaration
 	var latestFile;
 	var latestMod;
 	var fileName;
 	var concat;
 
+	// Switch filename
 	if (typeof file === 'string') {
 		fileName = file;
 	} else if (typeof file.path === 'string') {
 		fileName = path.basename(file.path);
 	} else {
-		throw new Error(PLUGIN_NAME+': Missing path in file options');
+		throw new PluginError(PLUGIN_NAME, 'Missing path in file options');
 	}
 
+	// Buffering function
 	function bufferContents(file, enc, cb) {
-		// ignore empty files
+		// Ignore empty files
 		if (file.isNull()) {
 			cb();
 			return;
@@ -155,19 +42,19 @@ function generatePOT(file) {
 
 		// Remove streams
 		if (file.isStream()) {
-			this.emit('error', new Error(PLUGIN_NAME+': Streaming not supported'));
+			this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported'));
 			cb();
 			return;
 		}
 
-		// set latest file if not already set,
+		// Set latest file if not already set,
 		// or if the current file was modified more recently.
 		if (!latestMod || file.stat && file.stat.mtime > latestMod) {
 			latestFile = file;
 			latestMod = file.stat && file.stat.mtime;
 		}
 
-		// construct concat instance
+		// Construct concat instance
 		if (!concat) {
 			concat = new Concat(false, fileName, '\n');
 		}
@@ -207,7 +94,7 @@ function generatePOT(file) {
 			// Remove last \n
 			holder = holder.substr(0, holder.length - 1);
 
-			// add file to concat instance
+			// Make new buffer from holder content and send it to concat instance
 			concat.add(file.relative, new Buffer(holder), file.sourceMap);
 
 			// Callback
@@ -220,26 +107,27 @@ function generatePOT(file) {
 		}
 	}
 
+	// Ending method, wrap the POT
 	function endStream(cb) {
-		// no files passed in, no file goes out
+		// No files passed in, no file goes out
 		if (!latestFile || !concat) {
 			cb();
 			return;
 		}
 
-		var joinedFile;
+		// Declare the final POT file
+		var potFile;
 
-		// if file opt was a file path
+		// If file opt was a file path
 		// clone everything from the latest file
 		if (typeof file === 'string') {
-			joinedFile = latestFile.clone({contents: false});
-			joinedFile.path = path.join(latestFile.base, file);
+			potFile = latestFile.clone({contents: false});
+			potFile.path = path.join(latestFile.base, file);
 		} else {
-			joinedFile = new File(file);
+			potFile = new File(file);
 		}
 
-		// Parse the content
-		// Translate in array and sort alphabetically
+		// Translate the concat in array and sort alphabetically
 		var tradArray = concat.content.toString().split("\n").sort();
 
 		// Remove duplicates and empty ones
@@ -250,16 +138,45 @@ function generatePOT(file) {
 			return uniques.hasOwnProperty(item) ? false : (uniques[item] = true);
 		});
 
-		// Build the POT File
-		var pot = utils.generatePotHeader() + '\nmsgid "' +
+		// Build the POT File content
+		var potContent = utils.generatePotHeader() + '\nmsgid "' +
 				uniques.join('"\nmsgstr ""\n\nmsgid "')
 				+ '"\nmsgstr ""\n';
 
-		// Push the joined file new content
-		joinedFile.contents = new Buffer(pot);
-		this.push(joinedFile);
+		// Set the POT file new content and push it downstream
+		potFile.contents = new Buffer(potContent);
+		this.push(potFile);
 		cb();
 	}
 
+	// Initialize the pipe
 	return through.obj(bufferContents, endStream);
-};
+}
+
+// Takes a potFile from the stream in and creates a .po file
+// Passes over the potFile to the stream
+function mergeAndMake(targetFile) {
+
+	if (!targetFile) {
+		throw new PluginError(PLUGIN_NAME, 'Target file path is required');
+	}
+
+	return through.obj(function (file, enc, callback) {
+
+		// Load the potFile from the stream
+		var potFile = gtxParser.po.parse(file.contents.toString());
+
+		// Makes PO and MO files from old PO and the new POT
+		utils.makePoMo(targetFile, potFile);
+
+		// Callback and passes over the original file
+		callback(null, file);
+
+	})
+}
+
+// Exporting the plugin main functions
+module.exports = {
+	mergeAndMake: mergeAndMake,
+	generatePOT: generatePOT
+}
